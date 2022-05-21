087eddb6f1a66e32806370233cec2bdb228800db
Vojta Netrh <vojtanetrh@gmail.com>
2022-05-21 14:33:35 +0200

    fucntion for subset test, v0.9 function subset_difference

--- ZPC2_zapocet_02/main.c
+++ ZPC2_zapocet_02/main.c
@@ -3,25 +3,36 @@
 #include <math.h>
 //#include "bitset.h"
 
+#define CHAR_BITS 8
+
 typedef struct bitset {
 	size_t size;
 	size_t relative_size;
 	unsigned char *set;
 } Bitset;
 
+void free_bitset(Bitset* set) {
+	for (int i = 0; i < set->size; i++) {
+		set->set[i] = NULL;
+		free(set->set[i]);
+	}
+	free(set->size);
+	free(set->relative_size);
+
+}
+
 Bitset* create_bitset(size_t size) {
 	Bitset* new_set = (Bitset*)malloc(sizeof(Bitset));
 	if (!new_set) {
-		free(new_set);
+		free_bitset(new_set);
 		return NULL;
 	}
 
-	size_t edited_size = ceil((double)size / 8);
+	size_t edited_size = ceil((double)size / CHAR_BITS);
 
 	new_set->set = (char*)malloc(edited_size * sizeof(char));
 	if (!new_set->set) {
-		free(new_set->set);
-		free(new_set);
+		free_bitset(new_set);
 		return NULL;
 	}
 
@@ -37,7 +48,7 @@ Bitset* create_bitset(size_t size) {
 Bitset* create_bitset_with_values(size_t size, const int* values, size_t array_size) {
 	Bitset* new_set = create_bitset(size);
 	if (!new_set) {
-		free(new_set);
+		free_bitset(new_set);
 		return NULL;
 	}
 
@@ -65,7 +76,7 @@ Bitset* create_bitset_with_values(size_t size, const int* values, size_t array_s
 Bitset* create_bitset_with_range(size_t size, int upto) {
 	Bitset* new_set = create_bitset(size);
 	if (!new_set) {
-		free(new_set);
+		free_bitset(new_set);
 		return NULL;
 	}
 
@@ -94,8 +105,8 @@ Bitset* create_bitset_with_range(size_t size, int upto) {
 }
 
 void set_element_change(Bitset* bitset, int element, int binary_value) {
-	int pos_in_array = element / 8;
-	int relative_pos = element % 8;
+	int pos_in_array = element / CHAR_BITS;
+	int relative_pos = element % CHAR_BITS;
 
 	int mask = 1 << (relative_pos);
 	bitset->set[pos_in_array] = (bitset->set[pos_in_array] & ~mask) | (binary_value << relative_pos);
@@ -110,8 +121,8 @@ void set_remove(Bitset* bitset, int element) {
 }
 
 int contains(Bitset* bitset, int element) {
-	int pos_in_array = element / 8;
-	int relative_pos = element % 8;
+	int pos_in_array = element / CHAR_BITS;
+	int relative_pos = element % CHAR_BITS;
 
 	if (((bitset->set[pos_in_array] >> relative_pos) & 1U) == 1) {
 		return 1;
@@ -146,6 +157,19 @@ char bitwise_substraction(char a, char b) {
 	return ((~b & a) & (b ^ a));
 }
 
+int bitwise_subset_test(char a, char b) {
+	/* 
+	 * A is a subset of B
+	 * alternatively we can use '(a & b) == a'
+	*/
+	if (bitwise_or(a, b) == b) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
 void form_operation(Bitset* left, Bitset* right, char (*bitwise_operation)(char, char)) {
 	for (int i = 0; i < right->relative_size; i++) {
 		left->set[i] = bitwise_operation(left->set[i], right->set[i]);
@@ -168,10 +192,7 @@ void form_intersection(Bitset* left, Bitset* right) {
 	form_operation(left, right, bitwise_and);
 }
 
-Bitset* set_operation(Bitset* left, Bitset* right, char (*bitwise_operation)(char, char)) {
-	size_t new_size = max(left->size, right->size);
-	size_t new_edited_size = ceil((double)new_size / 8);
-
+Bitset* set_operation(Bitset* left, Bitset* right, size_t new_size, size_t new_edited_size, char (*bitwise_operation)(char, char)) {
 	int* values = (int*)malloc(new_edited_size * sizeof(int));
 	if (!values) {
 		free(values);
@@ -182,7 +203,7 @@ Bitset* set_operation(Bitset* left, Bitset* right, char (*bitwise_operation)(cha
 	for (int i = 0; i < new_edited_size; i++) {
 		char temp_left = left->set[i];
 		char temp_right = right->set[i];
-		for (int j = 0; j < 8; j++) {
+		for (int j = 0; j < CHAR_BITS; j++) {
 			if (bitwise_operation((temp_left & 0x01), (temp_right & 0x01))) {
 				values[arr_size] = (i * 8) + j;
 				arr_size++;
@@ -196,7 +217,10 @@ Bitset* set_operation(Bitset* left, Bitset* right, char (*bitwise_operation)(cha
 }
 
 Bitset* set_intersection(Bitset* left, Bitset* right) {
-	return set_operation(left, right, bitwise_and);
+	size_t new_size = min(left->size, right->size);
+	size_t new_edited_size = ceil((double)new_size / CHAR_BITS);
+
+	return set_operation(left, right, new_size, new_edited_size, bitwise_and);
 }
 
 void form_union(Bitset *left, Bitset *right) {
@@ -204,7 +228,10 @@ void form_union(Bitset *left, Bitset *right) {
 }
 
 Bitset* set_union(Bitset *left, Bitset *right) {
-	return set_operation(left, right, bitwise_or);
+	size_t new_size = max(left->size, right->size);
+	size_t new_edited_size = ceil((double)new_size / CHAR_BITS);
+
+	return set_operation(left, right, new_size, new_edited_size, bitwise_or);
 }
 
 void form_difference(Bitset* left, Bitset* right) {
@@ -212,18 +239,49 @@ void form_difference(Bitset* left, Bitset* right) {
 }
 
 Bitset* set_difference(Bitset* left, Bitset* right) {
-	return NULL;
+	size_t new_size = left->size;
+	size_t new_edited_size = ceil((double)new_size / CHAR_BITS);
+
+	return set_operation(left, right, new_size, new_edited_size, bitwise_substraction);
+}
+
+int is_part_empty(Bitset* set, size_t start) {
+	for (size_t i = start; i < set->relative_size; i++) {
+		if (set->set[i] != 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int is_subset(Bitset* left, Bitset* right) {
+	size_t i = 0;
+	while (i < left->relative_size) {
+		if (bitwise_subset_test(left->set[i], right->set[i]) == 0) {
+			return 0;
+		}
+		if ((i + 1) > right->relative_size && left->relative_size > right->relative_size) {
+			if (is_part_empty(left, i + 1)) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+		i++;
+	}
+
+	return 1;
 }
 
 int main() {
 	char res = bitwise_substraction(40, 25);
-	int A[] = { 3, 5 };
-	int B[] = { 0, 3, 4 };				// 3, 5, 12, 15
-	Bitset* set_A = create_bitset_with_values(6, A, 2);
-	Bitset* set_B = create_bitset_with_values(5, B, 3);
+	int A[] = { 3, 5, 6 };
+	int B[] = { 3, 5, 6 };				// B - A : 0, 4						A - B : 8, 10, 12, 13
+	Bitset* set_A = create_bitset_with_values(20, A, 3);
+	Bitset* set_B = create_bitset_with_values(7, B, 3);
 
-	form_union(set_A, set_B); // 1, 2, 3, 5, 10 , 12, 14, 15
-	Bitset* set = set_union(set_A, set_B);
+	is_subset(set_A, set_B);
 
 	return 0;
 }
